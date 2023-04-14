884cbe5b071dd3cdf19b60c81beffa29acebae2b
Hasibur Rahman <hasiburdev@gmail.com>
2023-04-14 16:36:21 +0600

    Add users controller test code

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,12 +1,46 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { AuthService } from './auth.service';
+import { UsersService } from './users.service';
+import { User } from '@prisma/client';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let fakeUserService: Partial<UsersService>;
+  let fakeAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
+    fakeAuthService = {
+      signin: () => {
+        return Promise.resolve({
+          id: '123',
+          email: 'a@a.com',
+          password: '123',
+        } as User);
+      },
+      // signup: () => {},
+    };
+    fakeUserService = {
+      findByEmail: (email: string) => {
+        return Promise.resolve({ id: '123', email, password: '123' } as User);
+      },
+      findOne: (id) => {
+        return Promise.resolve({ id, email: '123', password: '123' } as User);
+      },
+      create: (email, password) => {
+        return Promise.resolve({ id: '123', email, password } as User);
+      },
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        { provide: UsersService, useValue: fakeUserService },
+        {
+          provide: AuthService,
+          useValue: fakeAuthService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
@@ -14,5 +48,15 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should update session object and return user', async () => {
+    const session = { userId: '' };
+    const user = await controller.signin(
+      { email: 'a@a.com', password: '123' },
+      session,
+    );
+    expect(user).toBeDefined();
+    expect(session?.userId).toEqual(user.id);
   });
 });
