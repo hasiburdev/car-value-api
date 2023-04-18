34772972a0b6d10f1adf4c0d3de384ff06de805a
Hasibur Rahman <hasiburdev@gmail.com>
2023-04-18 13:30:34 +0600

    Feat: Add admin guard and middleware

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -15,6 +15,7 @@ import { User } from '@prisma/client';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { ReportDto } from './dtos/report.dto';
 import { ApproveReportDto } from './dtos/approve-report.dto';
+import { AdminGuard } from '../guards/admin.guard';
 
 @Controller('reports')
 export class ReportsController {
@@ -29,12 +30,18 @@ export class ReportsController {
 
   @Patch('/:id')
   @UseGuards(AuthGuard)
+  @UseGuards(AdminGuard)
   @Serialize(ReportDto)
   approveReport(
     @Param('id') id: string,
     @CurrentUser() user: User,
     @Body() body: ApproveReportDto,
   ) {
-    return this.reportsService.changeApproval(id, user, body.approved);
+    try {
+      return this.reportsService.changeApproval(id, user, body.approved);
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
   }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,21 +1,16 @@
-import { Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
-import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 import { PrismaService } from '../prisma/prisma.service';
+import { CurrentUserMiddleware } from './middlewares/current-user.middleware';
 
 @Module({
   controllers: [UsersController],
-  providers: [
-    UsersService,
-    PrismaService,
-    AuthService,
-    {
-      useClass: CurrentUserInterceptor,
-      provide: APP_INTERCEPTOR,
-    },
-  ],
+  providers: [UsersService, PrismaService, AuthService],
 })
-export class UsersModule {}
+export class UsersModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CurrentUserMiddleware).forRoutes('*');
+  }
+}
